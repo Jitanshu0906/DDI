88d8333d74cd6f0cc55d379261cce0e32214e9db
Jitanshu Trivedi <136675080+Jitanshu0906@users.noreply.github.com>
2026-07-24 18:08:27 +0530

    Update Contact.tsx

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -294,8 +294,7 @@ export default function Contact() {
           <GlassCard className="overflow-hidden" data-testid="card-map">
             <div className="aspect-video">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.747282307725!2d72.77587729530892!3d21.150546938531757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04d8a36b354db%3A0xad7a3a030c453a30!2sVeneziano!5e1!3m2!1sen!2sin!4v1784896285183!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin">
-            </iframe>
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.747282307725!2d72.77587729530892!3d21.150546938531757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04d8a36b354db%3A0xad7a3a030c453a30!2sVeneziano!5e1!3m2!1sen!2sin!4v1784896285183!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
